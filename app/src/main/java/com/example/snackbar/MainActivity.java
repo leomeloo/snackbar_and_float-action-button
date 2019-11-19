9bec815af877c6b5b2a233f23663dddeb85053e8
Leonardo Melo <leonardo.jesus@hdi.com.br>
2019-11-19 16:23:14 -0300

    Snack Bar & Float Button ok

---
+++ app/src/main/java/com/example/snackbar/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingButton = findViewById(R.id.teste);
+
+        floatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Snackbar snackbar = Snackbar.make(view, "Essa é a SnackBar. VAI CORINTHIANS!", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Fechar", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        snackbar.dismiss();
+                    }
+                });
+
+                snackbar.show();
+            }
+        });
+    }
+}
